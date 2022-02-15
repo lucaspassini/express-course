530de37315b2e6def4455650b33c8de9eb22b5aa
Lucas Cristiano Passini <passini@protonmail.com>
2022-02-15 04:19:47 -0300

    add first GET route

--- server.js
+++ server.js
@@ -1,4 +1,14 @@
 const express = require('express');
 const app = express();
 
-app.listen(8000, () => console.log('server is running!'));
+const mockUserData = [{ name: 'Mark' }, { name: 'Jill' }];
+
+app.get('/users', (req, res) => {
+  res.json({
+    success: true,
+    message: 'successfully got users. Nice!',
+    users: mockUserData
+  });
+});
+
+app.listen(8000, () => console.log('server is listening'));
